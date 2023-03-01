12fdb3ed4cf3ef57e6f8574a9a436335fe63e838
leiderCastillo <leiderc25@gmail.com>
2023-03-01 17:02:43 -0500

    Ingresando Libros Prestados

--- AdministradorLibros/src/programa/Body.java
+++ AdministradorLibros/src/programa/Body.java
@@ -15,6 +15,7 @@ public class Body extends javax.swing.JFrame {
     public static bibliotecario Administrador;
     Color gris = new Color(238,238,238);
     Color naranja = new Color(255,204,0);
+    int codigoLibroPrestado = 0;
     
 
     public Body() {
@@ -97,13 +98,13 @@ public class Body extends javax.swing.JFrame {
         EntregarLibro = new javax.swing.JDialog();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        EntregarLibroCodigo = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jSeparator6 = new javax.swing.JSeparator();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
+        EntregaLibroNombreLibro = new javax.swing.JLabel();
+        EntregaLibroNombreUsuario = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        ConfirmarEntrega = new javax.swing.JButton();
         BarraMenu = new javax.swing.JPanel();
         TextAgregarLibro = new javax.swing.JLabel();
         BtnAgregarLibro = new javax.swing.JButton();
@@ -639,20 +640,30 @@ public class Body extends javax.swing.JFrame {
 
         jLabel3.setText("Codigo del Libro");
 
-        jLabel5.setText("Libro:");
+        EntregaLibroNombreLibro.setText("Libro:");
 
-        jLabel6.setText("Usuario:");
+        EntregaLibroNombreUsuario.setText("Usuario:");
 
         jButton1.setBackground(new java.awt.Color(255, 204, 0));
         jButton1.setText("Buscar");
         jButton1.setActionCommand("k");
         jButton1.setBorderPainted(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
-        jButton2.setBackground(new java.awt.Color(255, 204, 0));
-        jButton2.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
-        jButton2.setText("Confirmar");
-        jButton2.setActionCommand("k");
-        jButton2.setBorderPainted(false);
+        ConfirmarEntrega.setBackground(new java.awt.Color(255, 204, 0));
+        ConfirmarEntrega.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        ConfirmarEntrega.setText("Confirmar");
+        ConfirmarEntrega.setActionCommand("k");
+        ConfirmarEntrega.setBorderPainted(false);
+        ConfirmarEntrega.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ConfirmarEntregaActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -664,13 +675,13 @@ public class Body extends javax.swing.JFrame {
                     .addComponent(jLabel3)
                     .addComponent(jLabel2)
                     .addComponent(jSeparator6)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(EntregaLibroNombreLibro, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(EntregaLibroNombreUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(EntregarLibroCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1))
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(ConfirmarEntrega, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(19, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -682,16 +693,16 @@ public class Body extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(EntregarLibroCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator6, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel5)
+                .addComponent(EntregaLibroNombreLibro)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel6)
+                .addComponent(EntregaLibroNombreUsuario)
                 .addGap(33, 33, 33)
-                .addComponent(jButton2)
+                .addComponent(ConfirmarEntrega)
                 .addContainerGap(56, Short.MAX_VALUE))
         );
 
@@ -1013,7 +1024,10 @@ public class Body extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-
+        EntregarLibro.setVisible(true);
+        EntregarLibro.setLocationRelativeTo(this);
+        EntregarLibro.setSize(400,300);
+        ConfirmarEntrega.setEnabled(false);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void BtnAgregarLibroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnAgregarLibroActionPerformed
@@ -1333,6 +1347,37 @@ public class Body extends javax.swing.JFrame {
         
     }//GEN-LAST:event_MostrarDisponiblesActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        prestarLibro consulta = new prestarLibro();
+        int codigoLibro = 0;
+        try {
+            codigoLibro = Integer.parseInt(EntregarLibroCodigo.getText());
+            String val[] = consulta.recibirDatos(codigoLibro);
+            if(val[0] != null){
+                EntregaLibroNombreLibro.setText("Libro: "+val[0]);
+                EntregaLibroNombreUsuario.setText("Usuario: "+val[1]);
+                codigoLibroPrestado = Integer.parseInt(val[2]);
+                ConfirmarEntrega.setEnabled(true);
+            }else{
+                ConfirmarEntrega.setEnabled(false);
+                JOptionPane.showMessageDialog(EntregarLibro, "No se encontro el libro prestado");
+                
+            }
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(EntregarLibro, "El código no es un valor numérico");
+        }
+            
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void ConfirmarEntregaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConfirmarEntregaActionPerformed
+        prestarLibro consulta = new prestarLibro();
+        consulta.entregar(codigoLibroPrestado);
+        EntregarLibro.setVisible(false);
+        EntregaLibroNombreLibro.setText("Fecha: ");
+        EntregaLibroNombreUsuario.setText("Nombre: ");
+    }//GEN-LAST:event_ConfirmarEntregaActionPerformed
+
     public static void main(String args[]) {
         
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -1354,7 +1399,11 @@ public class Body extends javax.swing.JFrame {
     private javax.swing.JButton BtnPrestarLibro;
     private javax.swing.JButton BtnPrestarLibroAgregarUsuario;
     private javax.swing.JButton ButtonAgregarLibro;
+    private javax.swing.JButton ConfirmarEntrega;
+    private javax.swing.JLabel EntregaLibroNombreLibro;
+    private javax.swing.JLabel EntregaLibroNombreUsuario;
     private javax.swing.JDialog EntregarLibro;
+    private javax.swing.JTextField EntregarLibroCodigo;
     private javax.swing.JTextField FieldAgregarBibliClave;
     private javax.swing.JTextField FieldAgregarBibliId;
     private javax.swing.JTextField FieldAgregarBibliNombre;
@@ -1394,7 +1443,6 @@ public class Body extends javax.swing.JFrame {
     private javax.swing.JLabel TextPrestarLibro;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton17;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
@@ -1422,8 +1470,6 @@ public class Body extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel34;
     private javax.swing.JLabel jLabel35;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
@@ -1440,6 +1486,5 @@ public class Body extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator4;
     private javax.swing.JSeparator jSeparator5;
     private javax.swing.JSeparator jSeparator6;
-    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
--- AdministradorLibros/src/programa/OperacionesDB/prestarLibro.java
+++ AdministradorLibros/src/programa/OperacionesDB/prestarLibro.java
@@ -4,6 +4,7 @@ package programa.OperacionesDB;
 import java.sql.*;
 
 public class prestarLibro {
+    int idPrestarLibro;
     String fsalida;
     String fingreso;
     String idBibli;
@@ -11,7 +12,35 @@ public class prestarLibro {
     int idLibro;
     int entregado;
     Connection con;
+    
+    public void entregar(int i){
+        try {
+            String sql = "call entregarLibro("+i+")";
+            CallableStatement cmd = con.prepareCall(sql);
+            ResultSet rs = cmd.executeQuery();
+        }catch(Exception e){
+            System.out.println("Error en Entregar "+e);
+        }
+    }
 
+    public String[] recibirDatos(int codigo){
+        String valores[] = new String[3];
+        try {
+            String sql = "call obtenerValoresEntrega("+codigo+")";
+            CallableStatement cmd = con.prepareCall(sql);
+            ResultSet rs = cmd.executeQuery();
+            while(rs.next()){
+                valores[0] = rs.getString(1);
+                valores[1] = rs.getString(2);
+                valores[2] = rs.getString(3);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error en recibir Datos "+e);
+        }
+        return valores;
+    }
+    
     public prestarLibro(String fsalida, String fingreso, String idBibli, String idUser, int idLibro, int entregado) {
         this.fsalida = fsalida;
         this.fingreso = fingreso;
@@ -20,6 +49,10 @@ public class prestarLibro {
         this.idLibro = idLibro;
         this.entregado = entregado;
         con = coneccion.getConexion();
+    }
+    
+    public prestarLibro(){
+        con=coneccion.getConexion();
     }
     
     public void realizar(){
